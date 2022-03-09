37a171d3ff5ab4d42cf2d92b01c03fa6c76d6458
HBlaster <alfredo10396.c@gmail.com>
2022-03-08 18:25:07 -0600

    se agrega practica js de platzi

---
+++ porcentajes_1/descuento.js
@@ -0,0 +1,20 @@
+
+function CalcularDescuento (precio, descuento){
+    const PorcentajeDescuento = 100 - descuento;
+    const PrecioDescuento = (precio * PorcentajeDescuento)/100;
+
+    return PrecioDescuento;
+
+}
+
+/*function CalcDescuento() {
+    const l1 = document.getElementById("Input1"); 
+    const value1 = Number(l1.value);
+
+    const l2 = document.getElementById("Input2"); 
+    const value2 = Number(l2.value);
+
+    descuento = CalcularDescuento(value1, value2);
+
+    return descuento;
+}*/
