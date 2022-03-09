3cd46cca6cff739738a59241bebf10eb8e760bb1
HBlaster <alfredo10396.c@gmail.com>
2022-03-08 20:44:46 -0600

    se hace el calculo por medio de cupones con switch case

--- porcentajes/porcentajes.js
+++ porcentajes/porcentajes.js
@@ -7,7 +7,7 @@ function CalcularDescuento (precio, descuento){
 
 }
 
-function CalcDescuento() {
+/*function CalcDescuento() {
     const l1 = document.getElementById("Input1"); 
     const value1 = Number(l1.value);
 
@@ -18,4 +18,37 @@ function CalcDescuento() {
 
     const resul = document.getElementById("respuesta");
     resul.innerText = "El PRECIO CON DESCUENTO SON: $ " + descuento + " PESOS";
+} */
+
+const cupones = ["FIRST", "SECOND", "THIRD"];
+
+
+
+function CalcDescuento() {
+    const precio = document.getElementById("Input1");
+    const precio1 = Number(precio.value);
+
+    const cupon = document.getElementById("Input2");
+    const cuponValue = cupon.value;
+
+    switch (cuponValue){
+        case cupones [0]:
+            descuento = 50;
+        break;
+        case cupones [1]:
+            descuento = 30;
+        break;
+        case cupones [2]:
+            descuento = 10;
+        break;
+    }
+
+    preciofinal= CalcularDescuento (precio1, descuento);
+
+    const aplicado = document.getElementById("c");
+    aplicado.innerText = "El CUPON DE DESCUENTO QUE SE APLICO ES: '" + cuponValue + "' CON " + descuento + " % DE DESCUENTO!";
+
+    const resul = document.getElementById("respuesta");
+    resul.innerText = "El PRECIO CON DESCUENTO SON: $ " + preciofinal + " PESOS";
+    
 }
