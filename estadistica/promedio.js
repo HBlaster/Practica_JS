b24146c3f086fe2c6312be5ce2a26cea8ed9bcbd
HBlaster <alfredo10396.c@gmail.com>
2022-03-09 14:46:54 -0600

    se agrega codigo de js para calculo de media aritmetica

---
+++ estadistica/promedio.js
@@ -0,0 +1,25 @@
+const lista1 = [
+    100,
+    200,
+    300,
+    500
+];
+
+function MediaAritmetica(lista){
+/*let sumalista = 0;
+for (let i =0; i < lista.length; i++){
+    sumalista = sumalista + lista[i];
+} 
+*/
+
+const sumalista = lista.reduce (
+    function (ValorAcumulado = 0, nuevoEelemento){
+        return ValorAcumulado + nuevoEelemento;
+    }
+);
+
+
+let promedio = (sumalista)/lista.length;
+
+console.log (promedio); 
+}
