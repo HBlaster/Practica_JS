a949a8230557154ca0b0bf515da73cf31e79a5b9
HBlaster <alfredo10396.c@gmail.com>
2022-03-09 16:56:50 -0600

    se realizan funciones de media en js

--- estadistica/mediana.js
+++ estadistica/mediana.js
@@ -2,29 +2,58 @@ const lista1 = [
     100,
     200,
     300,
-    500000000,
-    5
+    500,
 
 ];
 
-const mitadLista1 = parseInt(lista1.length / 2);
+function MediaAritmetica(lista){
+    /*let sumalista = 0;
+    for (let i =0; i < lista.length; i++){
+        sumalista = sumalista + lista[i];
+    } 
+    */
+    
+    const sumalista = lista.reduce (
+        function (ValorAcumulado = 0, nuevoEelemento){
+            return ValorAcumulado + nuevoEelemento;
+        }
+    );
+    
+    
+    let promedio = (sumalista)/lista.length;
+    
+    return promedio;
+    
+}
 
 
-function esPar(numerito) {
+function calcularmediana (lista){
+    const mitadLista1 = parseInt(lista.length / 2);
+
+
+    function esPar(numerito) {
     if (numerito % 2 ===0 ){
         return true;
     }
     else{
         return false;
     }
+    }
+
+    let mediana ; 
+
+    if ( esPar(lista.length) ){
+
+        const elemento1 = lista[mitadLista1 - 1];
+        const elemento2 = lista[mitadLista1 ];
+
+        const promedio = MediaAritmetica([elemento1, elemento2]);
+        mediana = promedio;
+        console.log(mediana);
+
+
+    } else {
+        mediana = lista[mitadLista1];
+        console.log(mediana);
+    } 
 }
-
-let mediana = lista1[mitadLista1]; 
-
- if ( esPar(lista1.length) ){
-
-    console.log("Es par");
-} else {
-    mediana = lista1[mitadLista1];
-    console.log(mediana);
-} 
