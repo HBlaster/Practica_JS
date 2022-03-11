df7ff049c8f3e3c842163b8e1b5f8f6f3f7c6028
HBlaster <alfredo10396.c@gmail.com>
2022-03-10 22:18:22 -0600

    calculo salarial

--- analisis salarios/analisis.js
+++ analisis salarios/analisis.js
@@ -1,3 +1,28 @@
+// FUNCIONES HELPERS ******************************************
+
+// Se necesita saber si la cantidad de datos es par o inpar
+function esPar (numero){
+    return (numero % 2 == 0 );
+}
+
+//Funcion para calcular el promedio
+function MediaAritmetica(lista){
+  
+    const sumalista = lista.reduce (
+        function (ValorAcumulado = 0, nuevoEelemento){
+            return ValorAcumulado + nuevoEelemento;
+        }
+    );
+    
+    
+    let promedio = (sumalista)/lista.length;
+    
+    return promedio;
+    
+}
+
+//**************************************************************
+
 // Los datos que se necesitan para el analisis son unicamente los salarios de cada persona
 // entonces, se va a hacer un nuevo arreglo que solo guarde los salarios sin el nombre de cada persona
 
@@ -15,32 +40,14 @@ const SalarioOrdenado = SalariosMx.sort(
     }
 );
 
-// Se necesita saber si la cantidad de datos es par o inpar
-function esPar (numero){
-    return (numero % 2 == 0 );
-}
-
-function MediaAritmetica(lista){
-  
-    const sumalista = lista.reduce (
-        function (ValorAcumulado = 0, nuevoEelemento){
-            return ValorAcumulado + nuevoEelemento;
-        }
-    );
-    
-    
-    let promedio = (sumalista)/lista.length;
-    
-    return promedio;
-    
-}
-
+//  CALCULADORA DE MEDIANA
 //se hace el calculo de la mediana en funcion de si es par o inpar el numero de datos
 
 function medianaSalarios (lista){
     const mitad = parseInt(lista.length / 2);
     
     if(esPar(lista.length)){
+
         const personaMitad = lista[mitad - 1];
         const personaMitad1 = lista[mitad];
         const mediana = MediaAritmetica([personaMitad, personaMitad1]);
@@ -52,4 +59,22 @@ function medianaSalarios (lista){
     }
 }
 
-console.log(medianaSalarios(SalarioOrdenado));
+// CALCULADORA MEDIANA TOP 10%
+const spliceStart = (SalarioOrdenado.length*90)/100;
+const spliceCount = SalarioOrdenado.length - spliceStart;
+
+//El metodo splice permite "cortar" el arreglo en un punto y extraer una parte de lo seccionado
+const SalariosMxTop10 = SalarioOrdenado.splice(spliceStart, spliceCount,);
+
+
+//RESULTADOS
+const medianaGralMx = (medianaSalarios(SalarioOrdenado));
+const medianaTop10Mx = medianaSalarios(SalariosMxTop10);
+
+
+console.log({
+    medianaGralMx,
+    medianaTop10Mx,
+});
+
+
