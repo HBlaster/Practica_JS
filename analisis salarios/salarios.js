8e0b11ebc8626ae1fa326f810e7e587e3657539a
HBlaster <alfredo10396.c@gmail.com>
2022-03-10 18:01:15 -0600

    calculo de salarios

---
+++ analisis salarios/salarios.js
@@ -0,0 +1,123 @@
+const mexico = [];
+//con el metodo push se agregan a las personas y sus salarios dentro del arreglo "colombia"
+mexico.push (
+    {   
+        name: "natalia",
+        salary: 500, 
+    }
+);
+mexico.push (
+    {   
+        name: "maria",
+        salary: 200, 
+    }
+);
+mexico.push (
+    {   
+        name: "jose",
+        salary: 800, 
+    }
+);
+mexico.push (
+    {   
+        name: "alberto",
+        salary: 1000, 
+    }
+);
+mexico.push (
+    {   
+        name: "ana",
+        salary: 1200, 
+    }
+);
+mexico.push (
+    {   
+        name: "alfredo",
+        salary: 2500, 
+    }
+);
+mexico.push (
+    {   
+        name: "jorge",
+        salary: 1500, 
+    }
+);
+mexico.push (
+    {   
+        name: "rosa",
+        salary: 500, 
+    }
+);
+mexico.push (
+    {   
+        name: "jaun",
+        salary: 1500, 
+    }
+);
+mexico.push (
+    {   
+        name: "pedro",
+        salary: 700, 
+    }
+);
+mexico.push (
+    {   
+        name: "marta",
+        salary: 1500, 
+    }
+);
+mexico.push (
+    {   
+        name: "roberto",
+        salary: 450, 
+    }
+);
+mexico.push (
+    {   
+        name: "giovana",
+        salary: 800, 
+    }
+);
+mexico.push (
+    {   
+        name: "francisco",
+        salary: 1900, 
+    }
+);
+mexico.push (
+    {   
+        name: "mario",
+        salary: 2500, 
+    }
+);
+mexico.push (
+    {   
+        name: "ruben",
+        salary: 1500, 
+    }
+);
+mexico.push (
+    {   
+        name: "nora",
+        salary: 300, 
+    }
+);
+mexico.push (
+    {   
+        name: "luis",
+        salary: 950, 
+    }
+);
+mexico.push (
+    {   
+        name: "fernando",
+        salary: 3500, 
+    }
+);
+mexico.push (
+    {   
+        name: "blanca",
+        salary: 2500, 
+    }
+);
+
