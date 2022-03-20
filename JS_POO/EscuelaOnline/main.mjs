f31352b507364ee3dde0bf21bda98e755610cb90
HBlaster <alfredo10396.c@gmail.com>
2022-03-20 02:55:52 -0600

    actualizacion

--- JS_POO/EscuelaOnline/main.mjs
+++ JS_POO/EscuelaOnline/main.mjs
@@ -1,3 +1,46 @@
+function videoPlay(id){
+    const urlSecreta = "https://platziultrasecreto.com/" + id;
+    console.log("Se esta reproduciendo desd ela url" + urlSecreta );
+}
+
+function videoStop(id){
+    const urlSecreta = "https://platziultrasecreto.com/" + id;
+    console.log("Se pauso la url" + urlSecreta );
+}
+
+export class PlatziClass { 
+    constructor({
+        name,
+        videoID,
+    })
+    {
+         this.name = name;
+         this.videoID = videoID;   
+    }
+
+    reproducir()
+    {
+        videoPlay(this.videoID);
+    }
+
+    pausar()
+    {
+        videoStop(this.videoID);
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
 //Cursos de la plataforma
 class Courses {
     constructor({
---
+++ JS_POO/EscuelaOnline/module.mjs
@@ -0,0 +1,15 @@
+import {PlatziClass} from "./main.mjs"
+
+const clase76 = new PlatziClass({
+    name: "JavaScript: orientado a objetos basado en prototipos",
+    videoID: "sdf8746dfsg7d",
+});
+
+clase76.reproducir();
+clase76.pausar();
+
+clase76.reproducir();
+clase76.pausar();
+
+clase76.reproducir();
+clase76.pausar();
