48c66b4249c4825956ec245116b0988557420264
hblaster <alfredo10396.c@gmail.com>
2022-11-28 02:38:45 -0600

    api

--- manejo_arreglos/reduce_reloaded.js
+++ manejo_arreglos/reduce_reloaded.js
@@ -1,14 +1,54 @@
-const items = [1, 3, 2, 3];
+//Por medio del arreglo reduce se puede hacer un histograma con los elementos de un objeto
+// Ejemplo 1:
+/***********/
+// const items = [1, 3, 2, 3];
 
-const rta = items.reduce((obj, item) => {
+// const rta = items.reduce((obj, item) => {
 
-    if (!obj[item]) {
-        obj[item] = 1;
-    } else {
-        obj[item] = obj[item] + 1;
+//     if (!obj[item]) {
+//         obj[item] = 1;
+//     } else {
+//         obj[item] = obj[item] + 1;
+//     }
+//     return obj;
+
+// }, {});
+
+// console.log(rta); 
+
+//ejemplo 2:
+/***********/
+const data = [
+    {
+        name:"Nicolas",
+        level:"low"
+    },
+    {
+        name:"Alfredo",
+        level: "medium"
+    },
+    {
+        name:"Karla",
+        level:"hight"
+    },
+    {
+        name:"Martha",
+        level: "medium"
     }
-    return obj;
+];
 
-}, {});
+// Map crea un nuevo arreglo con el valor especificado
+const ans= data
+.map(item => item.level)
+.reduce((obj, item) => {
 
-console.log(rta); 
+        if (!obj[item]) {
+            obj[item] = 1;
+        } else {
+            obj[item] = obj[item] + 1;
+        }
+        return obj;
+    
+    }, {});;
+
+console.log(ans);
