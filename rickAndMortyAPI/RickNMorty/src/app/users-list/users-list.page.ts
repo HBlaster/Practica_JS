b0393d829f78cbbd49291467cfb2ed00d6706f9f
hblaster <alfredo10396.c@gmail.com>
2023-01-25 17:26:21 -0600

    practica api es6

---
+++ rickAndMortyAPI/RickNMorty/src/app/users-list/users-list.page.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Component({
+  selector: 'app-users-list',
+  templateUrl: './users-list.page.html',
+  styleUrls: ['./users-list.page.scss'],
+})
+export class UsersListPage implements OnInit {
+
+  characters = []
+  url = 'https://rickandmortyapi.com/api/character'
+
+
+  constructor(
+    private http: HttpClient,
+    
+    ) { }
+
+  ngOnInit() {
+    this.http.get<any>(this.url)
+    .subscribe(res=>{
+      this.characters = res.results;
+      console.log("Personajes:");
+      console.log(this.characters);
+    });
+  }
+
+}
